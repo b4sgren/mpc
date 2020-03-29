4be50687a4718f6cd18f48763404494815fbce84
Brendon Forsgren <bforsgren29@gmail.com>
2020-03-28 22:26:25 -0600

    have most of the controller written up. Will probably need to do some checking for optimality and stuff

---
+++ mpc.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import cvxpy as cp 
+
+class MPC:
+    def __init__(self, A, B, u_max, u_min, T=10): #Note that Q is diagonal and should only have values for states that I care to track (position and maybe heading)
+        self.A = A 
+        self.B = B 
+        self.u_max = u_max 
+        self.u_min = u_min 
+        self.Q = np.diag([1.0, 1.0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) #Weighting matrix for the states (x_ref - x_k)
+        self.R = np.eye(4) #Weighting matrix for the inputs (penalize high control effort)
+        self.T = T #Time horizon
+
+        self.n = self.A.shape[0] #Number of states 
+        self.m = self.B.shape[1] #Number of inputs
+    
+    def calculateControl(self, xr, x0): #xr (n x T+1) is the reference to track and x0 is the current state
+        x = cp.Variable((self.n, self.T+1), name="States")
+        u = cp.Variable((self.m, self.T), name="Inputs")
+
+        cost = 0
+        constr = []
+        phi_theta_max = np.array([np.pi/4, np.pi/4])
+        for t in range(self.T):
+            cost += cp.quad_form(xr[:,t+1] - x[:,t+1], self.Q) + cp.quad_form(u[:,t], self.R)
+            constr += [x[:,t+1] == self.A @ x[:,t] + self.B @ u[:,t]] 
+            constr += [x[6:8, t+1] <= phi_theta_max, x[6:8, t+1] >= -phi_theta_max] #Max constraint on roll and pitch angles
+            constr += [u[:,t] <= self.u_max , u[:,t] >= self.u_min] #Constraint on the inputs
+        constr += [x[:,0] == x0] # First state must be what is passed in
+
+        problem = cp.Problem(cp.Minimize(cost), constr)
+        problem.solve() #Will need to check if the soln is optimal
+        # problem.solve(solver=cp.ECOS)
+
+if __name__=="__main__":
+    debug = 1
